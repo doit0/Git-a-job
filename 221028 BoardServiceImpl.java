ad96bb9bdfa1e8b982c714da3e14047fd78ade52
kwondy9863 <112566454+doit0@users.noreply.github.com>
2022-11-01 23:09:47 +0900

    Update 221028 BoardServiceImpl.java

--- 221028 BoardServiceImpl.java
+++ 221028 BoardServiceImpl.java
@@ -25,8 +25,8 @@ public class BoardServiceImpl implements BoardService{
 	
 
 	// [1개의 게시판 글]을 리턴하는 메소드 선언				
-  /// 상세 DB 연동 간접 지시를 하는 작업 (세부적인 DB 연동 지시가 보이는 구문)
-  		/// 해야할 작업 1. 게시판 상세보기 작업을 할 때 조회수를 1 증가시켜줘야 함.  2. 게시판 글 가져오기
+	  /// 상세 DB 연동 간접 지시를 하는 작업 (세부적인 DB 연동 지시가 보이는 구문)
+  	  /// 해야할 작업 1. 게시판 상세보기 작업을 할 때 조회수를 1 증가시켜줘야 함.  2. 게시판 글 가져오기
   
 	public BoardDTO getBoard( int b_no,  boolean isBoardDetailForm ) {			
 		/// 2번째 인자값인 boolean이 true 일 경우에만 상세보기화면으로 들어가면 됨.
@@ -55,5 +55,66 @@ public class BoardServiceImpl implements BoardService{
 		return board;
 		
 	}
+	
+	
+	//--------------------------------------
+	// [1개의 게시판 글]을 수정 실행하고 수정 적용행의 개수를 리턴하는 메소드 선언
+	//--------------------------------------
+	public int updateBoard( BoardDTO boardDTO ) {			/// 게시판의 존재여부, 암호가 일치하는지 여부 등을 살펴봐야함.
+	
+		
+						
+		// 수정할 게시판의 개수 얻기 ( 만약 개수가 0개면 (= 이미 삭제된 거라면) 0 리턴하기 )
+		int boardCnt = this.boardDAO.getBoardCnt( boardDTO.getB_no() );			/// pk 값으로 게시판의 개수를 알아낼 수 있음.
+		if ( boardCnt == 0 ) {
+			return 0;				// boardUpDelForm  에서 삭제됐을 때는 0 리턴하기로 했으니까.
+		}
+						
+		// 암호의 존재 개수 얻기  ( 만약 수정할 게시글의 개수가 0개면 (= 암호가 틀렸다면) -1 리턴)	
+		int boardPwdCnt = this.boardDAO.getBoardPwdCnt( boardDTO );	
+		if ( boardPwdCnt ==0 ) {
+			return -1;				// boardUpDelForm  에서 암호가 불일치할 때는 -1 리턴하기로 했으니까.  (삭제여부를 먼저 살펴봐야 암호의 문제인지 삭제의 문제인지 알 수 있다)
+		}
+	
+		
+						
+		// 수정 실행하고 수정 적용행의 개수 얻기	
+		int updateBoardCnt = this.boardDAO.updateBoard( boardDTO );	
+			return updateBoardCnt; 
+	}
+	
+	
+	//-----------------------------------------------
+	// [1개의 게시판 글]을 삭제 후 삭제 적용행의 개수를 리턴하는 메소드 선언				
+	//-----------------------------------------------
+	public int  deleteBoard( BoardDTO boardDTO ) {				
+
+		// 삭제할 게시글의 개수 얻기 만약 삭제할 게시판의 개수가 0개면 (= 이미 삭제된 거라면) 0 리턴하기	
+		// System.out.println("boardService.getBoardCnt 메소드 호출!");
+		
+		int boardCnt = this.boardDAO.getBoardCnt( boardDTO.getB_no() );			/// pk 값으로 게시판의 개수를 알아낼 수 있음.
+		
+		if ( boardCnt == 0 ) {
+			return 0;				// boardUpDelForm  에서 삭제됐을 때는 0 리턴하기로 했으니까.
+		}
+				
+		// 암호의 존재 개수 얻기  ( 만약 수정할 게시글의 개수가 0개면 (= 암호가 틀렸다면) -1 리턴)						
+		//System.out.println("boardService.getboardDelPwdCnt 메소드 호출!");
+		
+		int boardPwdCnt = this.boardDAO.getBoardPwdCnt( boardDTO );	
+		
+		if ( boardPwdCnt == 0 ) {
+			return -1;				// boardUpDelForm  에서 암호가 불일치할 때는 -1 리턴하기로 했으니까.  (삭제여부를 먼저 살펴봐야 암호의 문제인지 삭제의 문제인지 알 수 있다)
+		}
+								
+		// 삭제 실행하고 삭제 적용행의 개수 얻기	
+		int deleteCnt = this.boardDAO.deleteBoard( boardDTO );	
+		//System.out.println("boardService.DeleteBoard 메소드 호출!");
+		
+		/// 삭제된 행을 제외한 출력순서 번호(print_no) 의 감소가 있어야함.
+			return deleteCnt; 		/// 1 리턴 예정
+	
+	}		//deleteBoard  끝나는 곳
+	
 
 }
